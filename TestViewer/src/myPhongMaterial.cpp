11ebd60d7096f932dd9c2ada20aa9238844ed86c
roub0002 <gael.roubahie-fissa@etudiant.univ-reims.fr>
2023-09-16 15:13:28 +0200

    Hope this is done, I want to carve my eyes out

--- TestViewer/src/myPhongMaterial.cpp
+++ TestViewer/src/myPhongMaterial.cpp
@@ -6,7 +6,7 @@ myPhongMaterial::myPhongMaterial( const QVector4D &ambient, const QVector4D &dif
     this->m_diffuse = diffuse;
     this->m_specpower = f;
 }
-
+ 
 void myPhongMaterial::render(const Mesh * mesh, const QGLCamera *c, const QList<PointLight> & lights ) {
     this->gl->glEnable(GL_DEPTH_TEST);
     this->m_program->bind();
@@ -20,15 +20,36 @@ void myPhongMaterial::render(const Mesh * mesh, const QGLCamera *c, const QList<
 
     this->m_program->setUniformValue("cameraPosition", c->getPosition());
 
-    for(PointLight light : lights) {
-        this->m_program->setUniformValue("lightDirection", light.getColor());
+    for(const PointLight &light : lights) {
+        this->m_program->setUniformValue("lightColor", light.getColor());
+        this->m_program->setUniformValue("lightPos", light.getPosition());
     }
-
-    QVector3D normal;
-
-    this->m_program->setUniformValue("cameraNormal", normal);
 
     mesh->getGeometry()->binds(this->attribPos, this->attribNorm, this->attribUvs, this->attribTangents);
     mesh->getGeometry()->draw();
     this->m_program->release();
+}
+
+QVector4D    myPhongMaterial::getAmbiant() const {
+    return this->m_ambiant;
+}
+
+void         myPhongMaterial::setAmbiant(const QVector4D &getAmbiant) {
+    this->m_ambiant = getAmbiant;
+}
+
+QVector4D    myPhongMaterial::getDiffuse() const {
+    return this->m_diffuse;
+}
+
+void         myPhongMaterial::setDiffuse(const QVector4D &getDiffuse) {
+    this->m_diffuse = getDiffuse;
+}
+
+float        myPhongMaterial::getSpecPower() const {
+    return this->m_specpower;
+}
+
+void         myPhongMaterial::setSpecpower(float getSpecPower) {
+    this->m_specpower = getSpecPower;
 }
